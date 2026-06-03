69c422f644c4889d89c70d24006d8d05431016b0
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 16:15:23 +0300

    feat/library: modif de la class Author dans le dossier entity

--- src/main/java/com/example/demo/entity/Author.java
+++ src/main/java/com/example/demo/entity/Author.java
@@ -1,10 +1,32 @@
 package com.example.demo.entity;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "author")
+@Getter
+@Setter
 public class Author {
-    private Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
     private String firstName;
     private String lastName;
-    private List<Book> books;
+    private String biography;
+    private String nationality;
+
+    public Author() {}
+
+    public Author(String id, String firstName, String lastName,
+                  String biography, String nationality) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.biography = biography;
+        this.nationality = nationality;
+    }
 }
