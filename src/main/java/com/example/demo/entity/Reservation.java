5e3c09c68f4649b0fdb89e062f5c5966854ad897
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 16:22:35 +0300

    feat/library: ajout de la class Reservation dans le dossier entity

---
+++ src/main/java/com/example/demo/entity/Reservation.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "reservation")
+@Getter
+@Setter
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private LocalDate reservationDate;
+    private LocalDate expirationDate;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToOne
+    @JoinColumn(name = "book_copy_id")
+    private BookCopy bookCopy;
+
+    public Reservation() {}
+
+    public Reservation(String id, LocalDate reservationDate, LocalDate expirationDate,
+                       ReservationStatus status, Customer customer, BookCopy bookCopy) {
+        this.id = id;
+        this.reservationDate = reservationDate;
+        this.expirationDate = expirationDate;
+        this.status = status;
+        this.customer = customer;
+        this.bookCopy = bookCopy;
+    }
+}
