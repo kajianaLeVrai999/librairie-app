d62068ccce72607b3f16a62824bb31ea6dedfd66
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 16:23:59 +0300

    feat/library: ajout de la class Sale dans le dossier entity

--- src/main/java/com/example/demo/entity/Sale.java
+++ src/main/java/com/example/demo/entity/Sale.java
@@ -1,11 +1,41 @@
 package com.example.demo.entity;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
 
+@Entity
+@Table(name = "sale")
+@Getter
+@Setter
 public class Sale {
-    private Long id;
-    private Date saleDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private LocalDate saleDate;
     private int quantity;
-    private double amount;
-    private Book book;
+    private Double totalAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "book_copy_id")
+    private BookCopy bookCopy;
+
+    public Sale() {}
+
+    public Sale(String id, LocalDate saleDate, int quantity,
+                Double totalAmount, Customer customer, BookCopy bookCopy) {
+        this.id = id;
+        this.saleDate = saleDate;
+        this.quantity = quantity;
+        this.totalAmount = totalAmount;
+        this.customer = customer;
+        this.bookCopy = bookCopy;
+    }
 }
