3b4c587664f85b029785362523c0ded7aaa35241
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 16:14:00 +0300

    feat/library: modif de la class Arrival dans le dossier entity

--- src/main/java/com/example/demo/entity/Arrival.java
+++ src/main/java/com/example/demo/entity/Arrival.java
@@ -1,10 +1,36 @@
 package com.example.demo.entity;
 
-import java.util.Date;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
 
+@Entity
+@Table(name = "arrival")
+@Getter
+@Setter
 public class Arrival {
-    private Long id;
-    private Date entryDate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private LocalDate arrivalDate;
     private int quantity;
-    private Book book;
+    private String supplier;
+
+    @ManyToOne
+    @JoinColumn(name = "bookCopy_id")
+    private BookCopy bookcopy;
+
+    public Arrival() {}
+
+    public Arrival(String id, LocalDate arrivalDate,
+                   int quantity, String supplier, BookCopy bookcopy) {
+        this.id = id;
+        this.arrivalDate = arrivalDate;
+        this.quantity = quantity;
+        this.supplier = supplier;
+        this.bookcopy = bookcopy;
+    }
 }
