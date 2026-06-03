9742012d8401e704f777342c33a32ae42142b5e2
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 16:17:21 +0300

    feat/library: modif de la class BookCopy dans le dossier entity

--- src/main/java/com/example/demo/entity/BookCopy.java
+++ src/main/java/com/example/demo/entity/BookCopy.java
@@ -1,12 +1,34 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "book_copy")
+@Getter
+@Setter
 public class BookCopy {
-    private Long id;
-    private String barcode;
-    private double price;
-    private boolean sold;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
     private Book book;
 
-    public boolean isAvailable();
-    public void markAsSold();
+    @Enumerated(EnumType.STRING)
+    private CopyStatus status;
+
+    private BookCopyFormat  bookCopyFormat;
+
+    public BookCopy() {}
+
+    public BookCopy(int id, Book book, CopyStatus status, BookCopyFormat bookCopyFormat) {
+        this.id = id;
+        this.book = book;
+        this.status = status;
+        this.bookCopyFormat = bookCopyFormat;
+    }
 }
