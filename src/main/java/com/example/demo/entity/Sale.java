6af6ea5e5186b8ea93dca655251501b26ab113ce
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 15:57:42 +0300

    feat/library: ajout de la class Sale dans le dossier entity

---
+++ src/main/java/com/example/demo/entity/Sale.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+public class Sale {
+    private Long id;
+    private Date saleDate;
+    private int quantity;
+    private double amount;
+    private Book book;
+}
