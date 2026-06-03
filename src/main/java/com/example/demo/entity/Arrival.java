77b6f91c1de9f04f916fee996a0d04edc63a01e5
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 15:55:15 +0300

    feat/library: ajout de la clas Arrival dans le dossier entity

---
+++ src/main/java/com/example/demo/entity/Arrival.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+public class Arrival {
+    private Long id;
+    private Date entryDate;
+    private int quantity;
+    private Book book;
+}
