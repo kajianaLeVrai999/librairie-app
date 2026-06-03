420cbd0df1915aabc377e1fe674f3bb572b97c41
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-03 15:55:44 +0300

    feat/library: ajout de la clas Author dans le dossier entity

---
+++ src/main/java/com/example/demo/entity/Author.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+public class Author {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private List<Book> books;
+}
