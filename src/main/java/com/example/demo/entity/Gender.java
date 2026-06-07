31e5f5be57514a93da87e0ac969f3ef41663207e
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-07 18:33:45 +0300

    develop: modif file Gender

--- src/main/java/com/example/demo/entity/Gender.java
+++ src/main/java/com/example/demo/entity/Gender.java
@@ -1,9 +1,16 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 import java.util.List;
 
+@Entity
 public class Gender {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @ManyToMany(mappedBy = "genderList")
     private List<Book> books;
 }
