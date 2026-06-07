ad5d6e63cc5114f736451c6fbf7089ed25e573f8
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-07 18:30:26 +0300

    develop: modif file Arrival

--- src/main/java/com/example/demo/entity/Arrival.java
+++ src/main/java/com/example/demo/entity/Arrival.java
@@ -1,36 +1,20 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
-@Table(name = "arrival")
-@Getter
-@Setter
 public class Arrival {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    private LocalDate arrivalDate;
+    @Temporal(TemporalType.DATE)
+    private Date entryDate;
+
     private int quantity;
-    private String supplier;
 
     @ManyToOne
     @JoinColumn(name = "book_id")
     private Book book;
-
-    public Arrival() {}
-
-    public Arrival(int id, LocalDate arrivalDate,
-                   int quantity, String supplier, Book book) {
-        this.id = id;
-        this.arrivalDate = arrivalDate;
-        this.quantity = quantity;
-        this.supplier = supplier;
-        this.book = book;
-    }
 }
