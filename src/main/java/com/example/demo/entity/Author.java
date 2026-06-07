adfd796eb676d20591d67a962f026856cfc174c3
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-07 18:31:15 +0300

    develop: modif file Author

--- src/main/java/com/example/demo/entity/Author.java
+++ src/main/java/com/example/demo/entity/Author.java
@@ -1,30 +1,17 @@
+package com.example.demo.entity;
+
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.List;
 
 @Entity
-@Table(name = "author")
-@Getter
-@Setter
 public class Author {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String firstName;
     private String lastName;
-    private String biography;
-    private String nationality;
 
-    public Author() {}
-
-    public Author(int id, String firstName, String lastName,
-                  String biography, String nationality) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.biography = biography;
-        this.nationality = nationality;
-    }
+    @ManyToMany(mappedBy = "authorList")
+    private List<Book> books;
 }
