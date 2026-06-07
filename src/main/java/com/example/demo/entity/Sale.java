4c16927d6599b900a7e68226f563a9d39f8cb100
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-07 18:34:28 +0300

    develop: modif file Sale

--- src/main/java/com/example/demo/entity/Sale.java
+++ src/main/java/com/example/demo/entity/Sale.java
@@ -1,41 +1,21 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
-@Table(name = "sale")
-@Getter
-@Setter
 public class Sale {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private LocalDate saleDate;
+    @Temporal(TemporalType.DATE)
+    private Date saleDate;
+
     private int quantity;
-    private Double totalAmount;
+    private double amount;
 
     @ManyToOne
-    @JoinColumn(name = "customer_id")
-    private Customer customer;
-
-    @ManyToOne
-    @JoinColumn(name = "book_copy_id")
-    private BookCopy bookCopy;
-
-    public Sale() {}
-
-    public Sale(String id, LocalDate saleDate, int quantity,
-                Double totalAmount, Customer customer, BookCopy bookCopy) {
-        this.id = id;
-        this.saleDate = saleDate;
-        this.quantity = quantity;
-        this.totalAmount = totalAmount;
-        this.customer = customer;
-        this.bookCopy = bookCopy;
-    }
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
