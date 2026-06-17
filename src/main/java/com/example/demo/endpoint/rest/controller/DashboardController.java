c397fe7fe33de72fb9d8aa883ce4e0236ce9f25f
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-06-17 11:47:21 +0300

    modif/preprod:ajout annotations dans le package controller et repository

--- src/main/java/com/example/demo/endpoint/rest/controller/DashboardController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/DashboardController.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DashboardController {
