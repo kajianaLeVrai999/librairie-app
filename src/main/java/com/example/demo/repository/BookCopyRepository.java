6808cd2d17a8c71f9be1a4822be41550e8399187
kajianaLeVrai999 <hei.kajiana@gmail.com>
2026-06-22 11:06:53 +0300

    fix: book repository

--- src/main/java/com/example/demo/repository/BookCopyRepository.java
+++ src/main/java/com/example/demo/repository/BookCopyRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.BookCopy;
 import com.example.demo.entity.CopyStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,9 +12,5 @@ public interface BookCopyRepository extends JpaRepository<BookCopy, Integer> {
 
   List<BookCopy> findByBookId(Integer bookId);
 
-  @Query(
-      """
-          select
-      """)
   long countByStatus(CopyStatus status);
 }
